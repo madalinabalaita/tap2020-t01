b68e699a1f86ebeffe34109ed817b899d56f5d9b
sorinsimco <sorinsimco@yahoo.com>
2020-02-27 18:59:37 +0200

    init file

---
+++ ssimco/source.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Imobil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
