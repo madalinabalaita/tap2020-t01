01e4d538e9b1760044c6543dd3e50fcb61541786
madalinabalaita <61286310+madalinabalaita@users.noreply.github.com>
2020-03-01 20:44:20 +0200

    Create Source.cs

---
+++ MadalinaBalaita/Source.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace madalinabalaita
+{
+    class Estate
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+
+        }
+    }
+}
