afabeb0edfbf2d1c95de42880709ae2ee626e5fd
madalinabalaita <61286310+madalinabalaita@users.noreply.github.com>
2020-03-03 22:42:46 +0200

    Add files via upload

---
+++ MadalinaBalaita/Flat.cs
@@ -0,0 +1,37 @@
+﻿namespace Imobile
+{
+    class Flat : Estate
+    {   
+        public Flat(decimal price1,int sm1,string location1,string condition1) : base(price1,sm1,location1,condition1)
+        {
+
+        }
+        public override decimal get_poundage()
+        {  //squares metres
+            if (sm < 15) throw new SquareMetersException();
+            if (sm < 43) price += 500m;
+            if (sm >= 43 && sm <= 59) price += 10000m;//apartament cu o camera
+            else if (sm >= 60 && sm <= 69) price += 17500m;//apartament cu 2 camere
+            else if (sm >= 70 && sm <= 90) price += 22500m;//apartament cu 3 camere
+
+            //location of the flat
+            if (location == "center") price += 5000m;
+            else if (location == "suburbs") price -= 2000m;
+            else if (location == "somewhere ok") price += 1000m;
+
+            //condition of the flat
+            if (condition == "really old") throw new UninhabitableException();
+            if (condition == "old") price -= 1000m;
+            else if (condition == "medium") price += 2000m;
+            else if (condition == "new") price += 5000m;
+
+            //poundage depending on the price
+            if (price < 30000m) return price * 0.035m;
+            if (price >= 30000m && price <= 50000m) return price * 0.05m;
+            else if (price > 50000m && price <= 70000m) return price * 0.08m;
+            return price * 0.1m;
+
+        }
+
+    }
+}
---
+++ MadalinaBalaita/House.cs
@@ -0,0 +1,43 @@
+﻿namespace Imobile
+{
+    class House :Estate
+    {
+        public House( decimal price,int sm1,string location1,string condition1) : base(price, sm1, location1,condition1)
+        {
+
+        }
+       /* public override decimal get_sm_price()
+        {  //square metres of the house
+           
+            return price;
+        }*/
+        public override decimal get_poundage()
+        {
+            if (sm < 15) throw new SquareMetersException();
+            if (sm < 70) price -= 500m;
+            if (sm >= 70 && sm <= 80) price += 1000m;
+            else if (sm > 80) price += 5000m;
+
+            //location of the house
+           if (location == "center") price += 15000m;
+            else if (location == "suburbs") price -= 6000m;
+            else if (location == "somewhere ok") price += 10000m;
+
+            //condition of the house
+            if (condition == "really old") throw new UninhabitableException();
+            if (condition == "old") price -= 2000m;
+            else if (condition == "medium") price += 6500m;
+            else if (condition == "new") price += 10000m;
+
+            //poundage depending on the price
+            if (price < 200000m) return price * 0.035m;
+            if (price >= 200000m && price <= 400000m) return price * 0.05m;
+            else if (price > 400000m && price <= 500000m) return price * 0.08m;
+            else return price * 0.1m;
+
+
+        }
+
+
+    }
+}
