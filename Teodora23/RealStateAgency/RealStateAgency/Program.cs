ef5b29cbe0b2b1d64453d3b2856110eee3121439
Teodora23 <47943153+Teodora23@users.noreply.github.com>
2020-03-04 14:55:52 +0200

    Update Program.cs

--- Teodora23/RealStateAgency/RealStateAgency/Program.cs
+++ Teodora23/RealStateAgency/RealStateAgency/Program.cs
@@ -19,17 +19,17 @@ namespace RealStateAgency
                 case "Apartment":
                     propertyPrice = Convert.ToDecimal(Console.ReadLine());
                     property = new Apartment(propertyPrice);
-                    Console.WriteLine("The final price is " + property.PropertySellingPrice + " from which the applied commission is " + property.Commission);
+                    Console.WriteLine("The final price is " + property.PropertySellingPrice + " from which the applied commission is " + property.Commission + ".");
                     break;
                 case "Single Room":
                     propertyPrice = Convert.ToDecimal(Console.ReadLine());
                     property = new SingleRoom(propertyPrice);
-                    Console.WriteLine("The final price is " + property.PropertySellingPrice + " from which the applied commission is " + property.Commission);
+                    Console.WriteLine("The final price is " + property.PropertySellingPrice + " from which the applied commission is " + property.Commission + ".");
                     break;
                 case "House":
                     propertyPrice = Convert.ToDecimal(Console.ReadLine());
                     property = new House(propertyPrice);
-                    Console.WriteLine("The final price is " + property.PropertySellingPrice + " from which the applied commission is " + property.Commission);
+                    Console.WriteLine("The final price is " + property.PropertySellingPrice + " from which the applied commission is " + property.Commission + ".");
                     break;
                 default:
                     throw new InvalidOperationException("This type does not exist!");
