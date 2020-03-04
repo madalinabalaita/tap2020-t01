c78476f7ce6b6895f666b88862ee0f2d6112b043
Teodora23 <47943153+Teodora23@users.noreply.github.com>
2020-03-04 14:42:35 +0200

    First attempt
    The program implements inheritance using a main class(Property) and a subclass(House)

---
+++ Teodora23/RealStateAgency/RealStateAgency/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RealStateAgency
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Property Type: ");
+            var propertyType = Console.ReadLine();
+            Console.WriteLine("Enter Initial Selling Price: ");
+            var propertyPrice = Convert.ToDecimal(Console.ReadLine());
+
+            Property property;
+
+            property = new House(propertyPrice);
+            Console.WriteLine("The final price is " + property.PropertySellingPrice + " from which the applied commission is " + property.Commission);
+        }
+    }
+}
---
+++ Teodora23/RealStateAgency/RealStateAgency/Property.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealStateAgency
+{
+    public abstract class Property
+    {
+        public decimal PropertySellingPrice { get; protected set; }
+
+        public decimal Commission { get; protected set; }
+
+        public decimal PropertyPrice { get; protected set; }
+
+        protected void CalculatePropertySellingPrice()
+        {
+            this.PropertySellingPrice = this.PropertyPrice + this.Commission;
+        }
+
+        protected abstract void CalculateCommission();
+    }
+}
