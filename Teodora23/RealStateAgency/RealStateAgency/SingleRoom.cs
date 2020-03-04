7d83eb403940b5ccd0112b2e3dfb5bc6ada9d532
Teodora23 <47943153+Teodora23@users.noreply.github.com>
2020-03-04 14:48:32 +0200

    Second Attempt
    Added subclasses Apartment and SingleRoom

---
+++ Teodora23/RealStateAgency/RealStateAgency/SingleRoom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealStateAgency
+{
+    class SingleRoom : Property
+    {
+        public SingleRoom(decimal propertyPrice)
+        {
+            this.PropertyPrice = propertyPrice;
+            this.CalculateCommission();
+            this.CalculatePropertySellingPrice();
+        }
+        protected override void CalculateCommission()
+        {
+            this.Commission = this.PropertyPrice * 3 / 100;
+        }
+
+    }
+}
