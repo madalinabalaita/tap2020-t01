34b6748e3bab004882d9fe6ce9ab11b7c4fab055
Crina-Otilia <50441486+Crina-Otilia@users.noreply.github.com>
2020-03-04 23:39:01 +0200

    Add files via upload

---
+++ PuriceCrina/Imobiliare.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    abstract class Imobiliare{
+    //We declare variables for the principal caracteristics of a real estate(imobiliar)
+        protected String location;
+        protected double surface;
+        protected String neighborhood;
+        protected int bathroom_number;
+        protected decimal price;
+    
+        public decimal Price { get { return price; } private set { this.price = value; } }
+
+        public void Change_Price(decimal money)
+        {
+        this.Price = money;
+        }
+
+        public void Selling_Price(decimal amount)
+        {
+            var fee = CalculateWithdrawalFee(amount);
+            amount = amount + fee;
+            if (Price <= amount)
+            {
+                throw new InvalidOperationException("Insufficient funds.");
+            }
+            this.Price -= amount;
+        }
+
+        protected abstract decimal CalculateWithdrawalFee(decimal amount);
+    }
+
---
+++ PuriceCrina/ImobiliareGarsoniere.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    class ImobiliareGarsoniere:Imobiliare
+    {
+        bool balcony;
+
+        public ImobiliareGarsoniere( String location, double surface, String neighborhood, int bathroom_number, bool balcony, decimal price)
+        {
+            this.location = location;
+            this.surface = surface;
+            this.neighborhood = neighborhood;
+            this.bathroom_number = bathroom_number;
+            this.balcony = balcony;
+            this.price = price;
+        }
+
+        protected override decimal CalculateWithdrawalFee(decimal amount)
+        {
+            return 0;
+        }
+
+
+    }
+
