d9624d5a3804c1d1e0e8c88d0c12aab3888365d8
madalinabalaita <61286310+madalinabalaita@users.noreply.github.com>
2020-03-04 18:56:44 +0200

    Update Program.cs

--- MadalinaBalaita/Program.cs
+++ MadalinaBalaita/Program.cs
@@ -1,5 +1,6 @@
 using System;
 
+
 namespace Imobile
 {
     public class SquareMetersException : Exception
@@ -20,54 +21,107 @@ namespace Imobile
       
         static void Main(string[] args)
         {
-            
-            try
-           {
-                House house = new House(200000m, 70, "center", "old");
-                Console.WriteLine("The price of the house is: " + house.get_price());
-            }
-             catch(SquareMetersException e)
-            {
-                Console.WriteLine("Not a lot of space to live in!", e);
-            }
-            catch (UninhabitableException e)
-            {
-                Console.WriteLine("A place too old to live in! Danger zone!", e);
-            }
+            string estate;
+            decimal initial_price;
+            int sqm;
+            string loc, cond;
+            Console.WriteLine("Welcome to our Real Estate Agency!");
+            Console.WriteLine("What are you looking for?(House/Flat/Studio)");
+            estate= Console.ReadLine();
+            Console.WriteLine(" ");
 
-            Flat flat1 = new Flat(58000m, 80, "somewhere ok", "new");
-            Console.WriteLine("The price of the a flat is: " + flat1.get_price());
-            try
+            if (estate == "House")
             {
-                Flat flat2 = new Flat(62000m,49, "suburbs", "really old");
-                Console.WriteLine("The price of the a flat is: " + flat2.get_price());
-            }
-            catch (SquareMetersException e)
-            {
-                Console.WriteLine("Not a lot of space to live in !", e);
-            }
-            catch (UninhabitableException e)
-            {
-                Console.WriteLine("A place too old to live in! Danger zone!", e);
-            }
+                Console.WriteLine("How much are you willing to pay?");
+                initial_price = decimal.Parse(Console.ReadLine());
+                Console.WriteLine(" ");
+                Console.WriteLine("How many square meters?");
+                sqm = int.Parse(Console.ReadLine());
+                Console.WriteLine(" ");
+                Console.WriteLine("Choose a location (center/somewhere ok/suburbs)");
+                loc = Console.ReadLine();
+                Console.WriteLine(" ");
+                Console.WriteLine("Choose the house condition(new/medium/old)");
+                cond = Console.ReadLine();
+                Console.WriteLine(" ");
+                try
+                {
+                    House house = new House(initial_price, sqm, loc, cond);
+                    Console.WriteLine("The price of this house(based on the price you are willing to pay) is: " + house.get_price_to_pay() + " with the poundage:" + house.get_poundage() + ", so the total price will be " + house.get_price());
 
-            Studio studio1 = new Studio(31000m, 20, "somewhere ok", "old");
-            Console.WriteLine("The price of the studio is: " + studio1.get_price());
-            try
+                }
+                catch (SquareMetersException e)
+                {
+                    Console.WriteLine("Not a lot of space to live in! Reconsider your option!", e);
+                }
+                catch (UninhabitableException e)
+                {
+                    Console.WriteLine("A place too old to live in! Danger zone!", e);
+                }
+            }
+            else if (estate == "Flat")
             {
+                Console.WriteLine("How much are you willing to pay?");
+                initial_price = decimal.Parse(Console.ReadLine());
+                Console.WriteLine(" ");
+                Console.WriteLine("How many square meters?");
+                sqm = int.Parse(Console.ReadLine());
+                Console.WriteLine(" ");
+                Console.WriteLine("Choose a location (center/somewhere ok/suburbs)");
+                loc = Console.ReadLine();
+                Console.WriteLine(" ");
+                Console.WriteLine("Choose the house condition(new/medium/old)");
+                cond = Console.ReadLine();
+                Console.WriteLine(" ");
+                try
+                {
+                    Flat flat = new Flat(initial_price, sqm, loc, cond);
+                    Console.WriteLine("The price of this flat(based on the price you are willing to pay) is: " + flat.get_price_to_pay() + " with the poundage:" + flat.get_poundage() + ", so the total price will be " + flat.get_price());
+                }
+                catch (SquareMetersException e)
+                {
+                    Console.WriteLine("Not a lot of space to live in! Reconsider your option!", e);
+                }
+                catch (UninhabitableException e)
+                {
+                    Console.WriteLine("A place too old to live in! Danger zone!", e);
+                }
+
+            }
+            else if (estate == "Studio")
+            {
+                Console.WriteLine("How much are you willing to pay?");
+                initial_price = decimal.Parse(Console.ReadLine());
+                Console.WriteLine(" ");
+                Console.WriteLine("How many square meters?");
+                sqm = int.Parse(Console.ReadLine());
+                Console.WriteLine(" ");
+                Console.WriteLine("Choose a location (center/somewhere ok/suburbs)");
+                loc = Console.ReadLine();
+                Console.WriteLine(" ");
+                Console.WriteLine("Choose the house condition(new/medium/old)");
+                cond = Console.ReadLine();
+                Console.WriteLine(" ");
+                try
+                {
+
+                    Studio studio = new Studio(initial_price, sqm, loc, cond);
+                    Console.WriteLine("The price of this studio(based on the price you are willing to pay) is: " + studio.get_price_to_pay() + " with the poundage:" + studio.get_poundage() + ", so the total price will be" + studio.get_price());
+                }
+                catch (SquareMetersException e)
+                {
+                    Console.WriteLine("Not a lot of space to live in! Reconsider your option!", e);
+                }
+                catch (UninhabitableException e)
+                {
+                    Console.WriteLine("A place too old to live in! Danger zone!", e);
+                }
                 
-                Studio studio2 = new Studio(30000m, 14, "somewhere ok", "old");
-                Console.WriteLine("The price of the studio is: " + studio2.get_price());
             }
-            catch(SquareMetersException e)
-            {
-                Console.WriteLine("Not a lot of space to live in !",e);
-            }
-            catch(UninhabitableException e)
-            {
-                Console.WriteLine("A place too old to live in! Danger zone!", e);
-            }
-
+               
+              
+               
+                Console.WriteLine("Thank you for choosing us! We hope you found what you were looking for!");
         }
     }
 }
