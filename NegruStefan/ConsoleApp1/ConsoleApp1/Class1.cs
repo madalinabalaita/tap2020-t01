dbb4bb91b974d61dfa0ed330812cea5e37e75134
stefanNegru <sn.s_48@list.ru>
2020-03-14 00:21:13 +0200

    tema1

---
+++ NegruStefan/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public abstract class Building
+    {
+        public decimal price;
+
+        public abstract decimal commission();
+        public decimal get_price()
+        {
+            return price + commission();
+        }
+    }
+
+    public class House : Building
+    {
+        public House(decimal price)
+        {
+            this.price = price;
+        }
+        public override decimal commission()
+        {
+            return 0.05m * price;
+        }
+    }
+
+    public class Apartment : Building
+    {
+        public Apartment(decimal price)
+        {
+            this.price = price;
+        }
+        public override decimal commission()
+        {
+            return 0.04m * price;
+        }
+    }
+
+    public class StudioApartment: Building
+    {
+        public StudioApartment(decimal price)
+        {
+            this.price = price;
+        }
+
+        public override decimal commission()
+        {
+            return 0.3m * price;
+        }
+    }
+}
---
+++ NegruStefan/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            ConsoleApp1.Building[] buildings = new ConsoleApp1.Building[20];
+            ConsoleApp1.House Casa1= new ConsoleApp1.House(150000);
+            ConsoleApp1.Apartment apartament1 = new ConsoleApp1.Apartment(35000);
+            ConsoleApp1.StudioApartment garsoniera1 = new ConsoleApp1.StudioApartment(15000);
+            buildings[0] = Casa1;
+            buildings[1] = apartament1;
+            buildings[2] = garsoniera1;
+            Console.WriteLine(buildings[0].get_price());
+        }
+    }
+}
